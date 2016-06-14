a59fc353452222e7dfe08652ddaca92fbf4f23eb
jalpesh vadgama <Jalpesh@dotnetjalps.com>
2016-06-14 11:53:25 +0530

    Initial Commit.

---
+++ CoreWebApi/src/CoreWebApi/Model/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreWebApi.Model
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Designation { get; set; }
+    }
+}
