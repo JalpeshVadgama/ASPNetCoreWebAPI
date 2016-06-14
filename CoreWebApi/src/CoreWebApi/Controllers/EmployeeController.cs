0a1aac1f47a4f1ac81f59592cd0bb31972f5011d
jalpesh vadgama <Jalpesh@dotnetjalps.com>
2016-06-14 12:03:36 +0530

    Created employee controller and added list.

---
+++ CoreWebApi/src/CoreWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using CoreWebApi.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+           var employees = new List<Employee>
+           {
+               new Employee {EmployeeId = 1,FirstName = "Jalpesh",LastName = "Vadgama",Designation = "Technical Architect"},
+               new Employee {EmployeeId = 2,FirstName = "Vishal",LastName = "Vadgama",Designation = "Technical Lead"}
+           };
+            return employees;
+        }
+    }
+}
